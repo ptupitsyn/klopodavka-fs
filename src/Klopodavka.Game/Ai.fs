ea58ff17c83bd3f446fc032969a22271288bbec1
Pavel <ptupitsyn@apache.org>
2019-11-08 15:19:44 +0300

    Exclude border tiles from random moves

--- src/Klopodavka.Game/Ai.fs
+++ src/Klopodavka.Game/Ai.fs
@@ -4,10 +4,12 @@ let makeRandomMove game =
     let tiles, w, h = Game.tilesAndSize game
     
     let isNotEmpty (x, y) = tiles.[x, y] <> Empty
+    let isNotBorder (x, y) = x > 0 && y > 0 && x < w - 1 && y < h - 1
     
     let hasOneNeighbor (x, y) =
         Board.neighbors w h x y
         |> Seq.filter isNotEmpty
+        |> Seq.filter isNotBorder
         |> Seq.filter (fun (x1, y1) -> x1 <> x && y1 <> y) // Diagonal
         |> Seq.length = 1
         
