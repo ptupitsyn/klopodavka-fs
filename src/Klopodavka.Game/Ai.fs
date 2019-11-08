526147ab08afe4c658d7d55945d61efd6c3e2b4f
Pavel <ptupitsyn@apache.org>
2019-11-08 15:52:21 +0300

    Implementing simple AI move

--- src/Klopodavka.Game/Ai.fs
+++ src/Klopodavka.Game/Ai.fs
@@ -1,9 +1,11 @@
 ﻿module Klopodavka.Game.Ai
+open Klopodavka.Game
 
 let makeRandomMove game =
-    let tiles, w, h = Game.tilesAndSize game
+    let board = game.Board
+    let w, h = Board.size board
     
-    let isNotEmpty (x, y) = tiles.[x, y] <> Empty
+    let isNotEmpty (x, y) = (Board.tile board x y) <> Empty
     let isNotBorder (x, y) = x > 0 && y > 0 && x < w - 1 && y < h - 1
     
     let hasOneNeighbor (x, y) =
@@ -13,7 +15,7 @@ let makeRandomMove game =
         |> Seq.length = 1
         
     let moves =
-        Board.moves game.Board game.CurrentPlayer
+        Board.moves board game.CurrentPlayer
         |> Seq.filter isNotBorder
         |> Seq.filter hasOneNeighbor
         |> Seq.toList
@@ -23,11 +25,23 @@ let makeRandomMove game =
         let idx = rnd.Next moves.Length
         let (x, y) = moves.Item idx
         Game.makeMove game x y
+        
+let makeSquashMove game =
+    let otherPlayer = Game.otherPlayer game.CurrentPlayer
+    let isSquashMove (x, y) = Board.tile game.Board x y = Alive otherPlayer
+    
+    let moves = Board.moves game.Board game.CurrentPlayer
+    let move =
+        moves
+        |> Seq.filter isSquashMove
+        |> Seq.head
+        
+    let x, y = move        
+    Game.makeMove game x y
 
 let makeAiMove game =
-    let tiles, w, h = Game.tilesAndSize game
     let tileSeq = Board.tileSeq game.Board
     let isSquashed tile = match tile with Squashed _ -> true | _ -> false
     let fightStarted = tileSeq |> Seq.exists isSquashed
         
-    fightStarted
+    if fightStarted then makeSquashMove game else makeRandomMove game
