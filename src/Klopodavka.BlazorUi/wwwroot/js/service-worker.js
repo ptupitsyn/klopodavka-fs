5a8e40c9df527a9184e8297dadbd32d9d87ab244
Pavel <ptupitsyn@apache.org>
2019-12-19 01:39:43 +0300

    Add service worker

---
+++ src/Klopodavka.BlazorUi/wwwroot/js/service-worker.js
@@ -0,0 +1,37 @@
+﻿const cacheName = 'offline-cache';
+let handleAsOfflineUntil = 0;
+
+self.addEventListener('install', async event => {
+    console.log('Installing service worker...');
+    await Promise.all((await caches.keys()).map(key => caches.delete(key)));
+    await (await caches.open(cacheName)).addAll(['loading.gif']);
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(getFromNetworkOrCache(event.request));
+});
+  
+async function getFromNetworkOrCache(request) {
+    if (new Date().valueOf() > handleAsOfflineUntil) {
+        try {
+            const networkResponse = await fetchWithTimeout(request, 1000);
+            (await caches.open(cacheName)).put(request, networkResponse.clone());
+            console.info('Fetched from network: ' + request.url);
+            return networkResponse;
+        } catch (ex) {
+            handleAsOfflineUntil = new Date().valueOf() + 3000; // Next 3 seconds
+        }
+    }
+
+    // Fall back on cache
+    console.info('Fetching from cache: ' + request.url);
+    return caches.match(request);
+}
+
+function fetchWithTimeout(request, timeoutMs) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => reject('Timed out'), timeoutMs);
+        fetch(request).then(resolve, reject);
+    });
+}
